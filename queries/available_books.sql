41df4ebb3a0b2c38eb02ef8ffa38dec5e56990c9
Rob <rowbypierre1998@gmail.com>
2024-01-21 18:12:16 -0500

    add column aggregation function for books with multiple authors to appear in one field oppose to duplicate book titles appear in

--- queries/available_books.sql
+++ queries/available_books.sql
@@ -1,12 +1,19 @@
 -- list of available books and author information
-select 		--*,
+with x as (
+	select 		--*,
 			title as "Book Title",
-			fname as "Author First Name",
-			lname as "Author Last Name",
-			mi as "Author MI"
-from 		book_authors  ba
+			concat(fname,' ', mi, '. ', lname) as "Full Name"
+	from 		book_authors  ba
 			left join authors a on ba.author_id = a.id
 			left join books b on ba.book_id = b.id 
 			left join statuses s on b.status_id = s.id
-where 		status in ('Available')
-order by 	title, fname, lname;
+	where 		status in ('Available')
+	group by 	"Full Name", "Book Title"
+	order by 	"Book Title"
+)
+
+select		x."Book Title",
+		string_agg("Full Name", ', ') as Author			
+from 		x 
+group by 	x."Book Title"
+order by 	x."Book Title"
