86c03c77d6e2ebaa0adba0dda149a53c150ed165
Rob <rowbypierre1998@gmail.com>
2024-01-21 18:36:26 -0500

    fix formatting

--- queries/average_book_condition.sql
+++ queries/average_book_condition.sql
@@ -1,8 +1,8 @@
 -- average condition of all books collection
 select 		condition "Condition",
-			count(*) "Count",
-			concat((count(*) * 100/(select count(*) from books)) , '% of Books') "Percent"
+		count(*) "Count",
+		concat((count(*) * 100/(select count(*) from books)) , '% of Books') "Percent"
 from		books b
-			left join conditions c on c.id = b.condition_id
+		left join conditions c on c.id = b.condition_id
 group by 	condition
 order by	"Count" desc, condition;
