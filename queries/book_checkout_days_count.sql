53fa0c39969d93bf1ab9f560d9b0015497055677
Rob <rowbypierre1998@gmail.com>
2024-02-04 17:02:27 -0500

    fix formatting

--- queries/book_checkout_days_count.sql
+++ queries/book_checkout_days_count.sql
@@ -1,18 +1,18 @@
 -- number of days a book has been checked out
 with x as (
-	select		*,
-				case 
-					when r.date is not null then r.date 
-					else current_date 
-				end as "Return Date",
-				case 
-					when c.date is not null then c.date
-					else current_date
-				end as "Checkout Date"
-	from		books b
-				left join loans l  on b.id = l.book_id 
-				left join returns r on r.id = l.return_id
-				left join checkouts c on c.id = l.checkout_id
+	select	*,
+		case 
+			when r.date is not null then r.date 
+			else current_date 
+		end as "Return Date",
+		case 
+			when c.date is not null then c.date
+			else current_date
+		end as "Checkout Date"
+	from	books b
+		left join loans l  on b.id = l.book_id 
+		left join returns r on r.id = l.return_id
+		left join checkouts c on c.id = l.checkout_id
 ) 
 
 select 		title "Book Title",
