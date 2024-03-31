e7257519528ae93648af876fc2c1d238dfc6a536
Rob <rowbypierre1998@gmail.com>
2024-03-31 05:03:44 -0400

    reformat

--- queries/overdue_returns_summary.sql
+++ queries/overdue_returns_summary.sql
@@ -1,17 +1,17 @@
--- overdue returns 
+-- Overdue returns. 
 with x as ( 
-	select 	concat(p.fname, ' ', p.lname) as "Patron",
-		b.title "Book",
-		c.date "Checkout",
-		l.due "Due",
-		r.date "Returned",
-		(r.date - l.due) "Days Overdue",
-		concat('$',r.fine) "Fine"
+	select 	concat(p.fname, ' ', p.lname)  		"Patron",
+			b.title 							"Book",
+			c.date 								"Checkout",
+			l.due 								"Due",
+			r.date 								"Returned",
+			(r.date - l.due) 					"Days Overdue",
+			concat('$',r.fine) 					"Fine"
 	from 	loans l
-		join checkouts c on c.id = l.checkout_id
-		join returns r on r.id = l.return_id
-		join books b on b.id = l.book_id
-		join patrons p on p.id = l. patron_id
+	join 	checkouts c on c.id = l.checkout_id
+	join 	returns r on r.id = l.return_id
+	join 	books b on b.id = l.book_id
+	join 	patrons p on p.id = l. patron_id
 )
 
 select 		* 
