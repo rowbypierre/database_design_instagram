29075fffd8b28211a86a7180b4bf358e7efc01c0
Rob <rowbypierre1998@gmail.com>
2024-03-31 04:48:34 -0400

    reformat

--- queries/available_books.sql
+++ queries/available_books.sql
@@ -1,19 +1,19 @@
--- list of available books and author information
+-- List of available books and author information.
 with x as (
 	select 		--*,
-			title as "Book Title",
-			concat(fname,' ', mi, '. ', lname) as "Full Name"
+				title as "Book Title",
+				concat(fname,' ', mi, '. ', lname) as "Full Name"
 	from 		book_authors  ba
-			left join authors a on ba.author_id = a.id
-			left join books b on ba.book_id = b.id 
-			left join statuses s on b.status_id = s.id
+	left join 	authors a on ba.author_id = a.id
+	left join 	books b on ba.book_id = b.id 
+	left join 	statuses s on b.status_id = s.id
 	where 		status in ('Available')
 	group by 	"Full Name", "Book Title"
 	order by 	"Book Title"
 )
 
 select		x."Book Title",
-		string_agg("Full Name", ', ') as Author			
+			string_agg("Full Name", ', ') as Author			
 from 		x 
 group by 	x."Book Title"
 order by 	x."Book Title"
