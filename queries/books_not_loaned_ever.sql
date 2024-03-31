55ce6c91c5bd427bdebb9ca44712acc8ae053af5
Rob <rowbypierre1998@gmail.com>
2024-03-31 04:57:30 -0400

    reformat

--- queries/books_not_loaned_ever.sql
+++ queries/books_not_loaned_ever.sql
@@ -1,18 +1,18 @@
 -- list of available books and author information
 with x as (
 	select 		--*,
-			title as "Book Title",
-			concat(fname,' ', mi, '. ', lname) as "Full Name"
+				title  									"Book Title",
+				concat(fname,' ', mi, '. ', lname)  	"Full Name"
 	from 		book_authors  ba
-			left join authors a on ba.author_id = a.id
-			left join books b on ba.book_id = b.id 
-			left join statuses s on b.status_id = s.id
+	left join 	authors a on ba.author_id = a.id
+	left join 	books b on ba.book_id = b.id 
+	left join 	statuses s on b.status_id = s.id
 	where 		b.id not in (select book_id from loans)
 	order by 	"Book Title"
 )
 
-select		x."Book Title",
-		string_agg("Full Name", ', ') as Author			
+select										x."Book Title",
+			string_agg("Full Name", ', ')  	Author			
 from 		x 
 group by 	x."Book Title"
 order by 	x."Book Title"
