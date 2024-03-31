72fbabbc6991ddccc59038639bd0b2530bad35e5
Rob <rowbypierre1998@gmail.com>
2024-03-31 04:55:53 -0400

    reformat

--- queries/book_collection.sql
+++ queries/book_collection.sql
@@ -1,8 +1,12 @@
 -- general overview of the library's book collection
-select 			--*,
-			title "Title", isbn "ISBN", genre "Genre", condition "Condition"
-from			books b
-			left join genres g on g.id = b.genre_id 
-			left join conditions c on c.id = b.condition_id
-			left join statuses s on s.id = b.status_id
-order by		Genre, Title;
+select 		--*,
+			title 		"Title"
+			,ISBN 		"ISBN"
+			,genre 		"Genre"
+			,condition 	"Condition"
+from		books b
+left join 	genres g on g.id = b.genre_id 
+left join 	conditions c on c.id = b.condition_id
+left join 	statuses s on s.id = b.status_id
+order by	Genre 
+			,Title;
