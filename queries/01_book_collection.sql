c7a5c95b329711ad1ac34db690dc7122c70f139b
Rowby Pierre <rowbypierre1998@gmail.com>
2024-04-18 11:43:07 -0400

    replace column names in clause with column positions

--- queries/01_book_collection.sql
+++ queries/01_book_collection.sql
@@ -1,12 +1,25 @@
 -- General overview of the library's book collection.
 select 		--*,
-			title 		"Title"
-			,ISBN 		"ISBN"
-			,genre 		"Genre"
-			,condition 	"Condition"
+			b.title 		            "Title"
+			,b.ISBN 		            "ISBN"
+            ,string_agg(concat(a.fname,
+                             ' ',
+                             upper(mi),
+                             '. ',
+                             a.lname) 
+                        ,', ')          "Author(s)"
+			,g.genre 		            "Genre"
+			,c.condition 	            "Condition"
 from		books b
+left join   book_authors ba ON ba.book_id = b.id 
+left join   authors a on ba.author_id = a.id
 left join 	genres g on g.id = b.genre_id 
 left join 	conditions c on c.id = b.condition_id
 left join 	statuses s on s.id = b.status_id
-order by	Genre 
-			,Title;
+group by    1,
+            2,
+            4,
+            5
+order by	1;
+
+select * from authors;
--- queries/09_staff_dbms_activity.sql
+++ queries/09_staff_dbms_activity.sql
@@ -1,33 +1,35 @@
 -- Staff count of records creations in database system. 
 with x as (
-	select		created_staff_id "ID", 
-				case when 1=1 then 'Books' end as 	"Record Type"
+	select		created_staff_id                    "ID", 
+				case when 1=1 then 'Books' end 		"Record Type"
 	from		books
 	union
-	select		created_staff_id "ID", 
+	select		created_staff_id                    "ID", 
 				case when 1=1 then 'Checkouts' end	"Record Type"
 	from		checkouts
 	union
-	select		created_staff_id "ID", 
+	select		created_staff_id                    "ID", 
 				case when 1=1 then 'Loans' end		"Record Type"
 	from		loans
 	union
-	select		created_staff_id "ID", 
+	select		created_staff_id                    "ID", 
 				case when 1=1 then 'Patrons' end  	"Record Type"
 	from		patrons
 	union
-	select		created_staff_id "ID", 
+	select		created_staff_id                    "ID", 
 				case when 1=1 then 'Returns' end  	"Record Type"
 	from		returns
 	union
-	select		created_staff_id "ID", 
+	select		created_staff_id                    "ID", 
 				case when 1=1 then 'Staff' end		"Record Type"
 	from		staff 			
 )
 
-select 		concat(s.fname, ' ', s.lname) "Name"
-			,count("Record Type") "Count"
-from		x
+select 		concat(s.fname, ' ', s.lname) 	"Staff Name"
+            ,r.role                         "Position/ Title"
+            ,count(2) 			            "Record(s) Created"
+            from		x
 join 		staff s on s.id = x."ID" 
-group by 	"Name"
-order by 	"Name";
+join        roles r on r.id = s.role_id
+group by 	1,2
+order by 	3 desc, 1;
