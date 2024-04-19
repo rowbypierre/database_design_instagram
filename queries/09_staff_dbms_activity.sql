bdd682031337734fd872751af865dc9019592fe0
Rowby Pierre <rowbypierre1998@gmail.com>
2024-04-18 20:13:24 -0400

    update staff activity query change union to union all

--- queries/09_staff_dbms_activity.sql
+++ queries/09_staff_dbms_activity.sql
@@ -3,23 +3,23 @@ with x as (
 	select		created_staff_id                    "ID", 
 				case when 1=1 then 'Books' end 		"Record Type"
 	from		books
-	union
+	union all
 	select		created_staff_id                    "ID", 
 				case when 1=1 then 'Checkouts' end	"Record Type"
 	from		checkouts
-	union
+	union all
 	select		created_staff_id                    "ID", 
 				case when 1=1 then 'Loans' end		"Record Type"
 	from		loans
-	union
+	union all
 	select		created_staff_id                    "ID", 
 				case when 1=1 then 'Patrons' end  	"Record Type"
 	from		patrons
-	union
+	union all
 	select		created_staff_id                    "ID", 
 				case when 1=1 then 'Returns' end  	"Record Type"
 	from		returns
-	union
+	union all
 	select		created_staff_id                    "ID", 
 				case when 1=1 then 'Staff' end		"Record Type"
 	from		staff 			
@@ -28,8 +28,9 @@ with x as (
 select 		concat(s.fname, ' ', s.lname) 	"Staff Name"
             ,r.role                         "Position/ Title"
             ,count(2) 			            "Record(s) Created"
+            ,s.hire                         "Onboard Date"
             from		x
 join 		staff s on s.id = x."ID" 
 join        roles r on r.id = s.role_id
-group by 	1, 2
+group by 	1, 2, 4
 order by 	3 desc, 1;
